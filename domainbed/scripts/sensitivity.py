b258210e8e9d1a905789c2a3a281ad54a60d0b00
Henry Gouk <henry.gouk@gmail.com>
2025-08-03 23:21:09 +0100

    Send algorithm to correct device

--- domainbed/scripts/sensitivity.py
+++ domainbed/scripts/sensitivity.py
@@ -81,6 +81,7 @@ def main():
             tr = [l for j, l in enumerate(train_loaders) if j != i]
             minibatches_iterator = zip(*tr)
             algorithm = algorithm_class(dataset.input_shape, dataset.num_classes, len(tr), hparams)
+            algorithm.to(device)
             
             for step in range(args.max_steps):
                 minibatches = [(x.to(device), y.to(device)) for x, y in next(minibatches_iterator)]
