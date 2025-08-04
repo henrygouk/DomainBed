6e2813909301db45bcc0f8f063e6e1c47c072e35
Henry Gouk <henry.gouk@gmail.com>
2025-08-04 01:26:52 +0100

    Change from decay_stu to decay_tec based on ablation in the RES paper

--- domainbed/scripts/sensitivity.py
+++ domainbed/scripts/sensitivity.py
@@ -98,8 +98,9 @@ def main():
     hparams = hparams_registry.default_hparams(args.algorithm, args.dataset)
 
     if args.algorithm == "RES":
-        hparam_name = "decay_stu"
-        hparam_values = [0.0, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
+        hparam_name = "decay_tec"
+        hparam_values = [1.0, 0.5, 0.25, 0.125, 0.0625, 0.03125, 0.015625, 0.0078125, 0.00390625, 0.001953125, 0.0009765625]
+        hparams["decay_stu"] = 0.0
     else:
         raise ValueError(f"Algorithm {args.algorithm} not supported for sensitivity analysis.")
 
